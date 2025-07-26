d8642734580ff3d51bd597accc928a6bf2f864a8
BALIRAM TODKARI <t.baliram.todkari@accenture.com>
2025-07-27 01:33:53 +0530

    Added health check endpoint

--- APIs.js
+++ APIs.js
@@ -9,6 +9,10 @@ const verifyToken = require('./middlewere/verifyAccessToken');
 router.route('/logout').post(logout.endpoint);
 router.route('/refresh').post(refresh.endpoint);
 router.route('/login').post(login.endpoint);
+router.route('/health').get((req, res) => {
+  res.status(200).json({ message: 'service is healthy' });
+});
+
 router.route('/protected').get(verifyToken, (req, res) => {
   console.log('Protected route accessed', req.body);
   res.status(200).json({ message: 'Welcome to the API', user: req.user || 'Guest' });
